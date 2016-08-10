f0000f0a5d5d6879c0e823c445181cd29a84e166
joestephens <joe.stephens@live.co.uk>
2016-08-10 11:18:53 +0100

    wrote 2 failing tests for statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Statement do
 
   subject(:statement) { described_class.new }
 
+  context 'when a statement is initialised' do
+    it 'has no transactions' do
+      expect(statement.transactions).to eq([])
+    end
+  end
+
+  context 'when a client makes a transaction' do
+    it 'it is added to their statement' do
+      now = Time.now
+      statement.add_transaction(now, 1000)
+      expect(statement.transactions).to include([now, 1000])
+    end
+  end
+
 end
