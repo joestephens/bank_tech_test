7581baeafff6d62ccd7513b4cba8edb5b0212620
joestephens <joe.stephens@live.co.uk>
2016-08-10 11:04:49 +0100

    added another failing test and code to pass for adding clients

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,9 +1,14 @@
+# understands having clients
 class Bank
 
   attr_reader :clients
 
   def initialize
     @clients = []
+  end
+
+  def add_client(client)
+    @clients << client
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -4,10 +4,21 @@ RSpec.describe Bank do
 
   subject(:bank) { described_class.new }
 
+  let(:client) { double(:client) }
+
   context 'when a bank is created' do
 
     it 'it has no clients' do
       expect(bank.clients).to eq([])
+    end
+
+  end
+
+  context 'when somebody wants to join the bank' do
+
+    it 'the bank can add them as a client' do
+      bank.add_client(client)
+      expect(bank.clients).to eq([client])
     end
 
   end
