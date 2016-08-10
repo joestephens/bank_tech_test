62991342fbcb75114bb8832625254688320754e6
joestephens <joe.stephens@live.co.uk>
2016-08-10 10:43:23 +0100

    added another failing test

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,7 +7,16 @@ RSpec.describe Client do
   context 'when a new client is created' do
 
     it 'they should have a starting balance of 0' do
-      expect(client.balance).to eq 0
+      expect(client.balance).to eq(0)
+    end
+
+  end
+
+  context 'when a client wants to adjust their balance' do
+
+    it 'they can make a deposit' do
+      client.deposit(1000)
+      expect(client.balance).to eq(1000)
     end
 
   end
